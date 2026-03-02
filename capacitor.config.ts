2845a4f95e9e08d9a240cf7f96767f2d8b5e993f
sabuat <sabuaturbina@gmail.com>
2026-03-02 07:50:50 -0300

    Google Error

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,9 +4,15 @@ const config: CapacitorConfig = {
   appId: 'com.sabuat.apapacho',
   appName: 'Apapacho',
   webDir: 'out',
+  // @ts-ignore
   plugins: {
     StatusBar: {
-      hidden: true, // <-- ORDEN MILITAR: Ocultar la barra superior
+      hidden: true,
+    },
+    GoogleAuth: {
+      scopes: ['profile', 'email'],
+      serverClientId: 'PEGA_AQUI_TU_WEB_CLIENT_ID', // <-- ¡Acuérdate de pegar tu código real aquí!
+      forceCodeForRefreshToken: true,
     }
   }
 };
